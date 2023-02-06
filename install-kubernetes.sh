8a798e7bb0f26cdc62d91d261fd02bd6e11c93b3
Ryan Dellolio <ryan@dellol.io>
2023-02-05 22:12:31 -0500

    Update install-kubernetes.sh

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-# Download necessary binaries
-cd /tmp
-curl -LO https://dl.k8s.io/release/stable.txt
-curl -L https://dl.k8s.io/release/$(cat stable.txt)/bin/linux/amd64/kubectl
-chmod +x kubectl
-sudo mv kubectl /usr/local/bin/
-curl -L https://dl.k8s.io/release/$(cat stable.txt)/bin/linux/amd64/kubelet
-chmod +x kubelet
-sudo mv kubelet /usr/local/bin/
-curl -L https://dl.k8s.io/release/$(cat stable.txt)/bin/linux/amd64/kubeadm
-chmod +x kubeadm
-sudo mv kubeadm /usr/local/bin/
+CNI_PLUGINS_VERSION="v1.1.1"
+ARCH="amd64"
+DEST="/opt/cni/bin"
+sudo mkdir -p "$DEST"
+curl -L "https://github.com/containernetworking/plugins/releases/download/${CNI_PLUGINS_VERSION}/cni-plugins-linux-${ARCH}-${CNI_PLUGINS_VERSION}.tgz" | sudo tar -C "$DEST" -xz
 
-# Start the kubelet service
-sudo su
-echo 'Environment="KUBELET_EXTRA_ARGS=--fail-swap-on=false"' > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-systemctl daemon-reload
-systemctl enable kubelet
-systemctl start kubelet
+DOWNLOAD_DIR="/opt/bin"
+sudo mkdir -p "$DOWNLOAD_DIR"
 
-# Initialize the cluster
-kubeadm init --pod-network-cidr=10.244.0.0/16
+CRICTL_VERSION="v1.25.0"
+ARCH="amd64"
 
-# Configure kubectl
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+curl -L "https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz" | sudo tar -C $DOWNLOAD_DIR -xz
 
-# Deploy a pod network
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
+ARCH="amd64"
+cd $DOWNLOAD_DIR
+sudo curl -L --remote-name-all https://dl.k8s.io/release/${RELEASE}/bin/linux/${ARCH}/{kubeadm,kubelet}
+sudo chmod +x {kubeadm,kubelet}
+
+RELEASE_VERSION="v0.4.0"
+curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubelet/lib/systemd/system/kubelet.service" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | sudo tee /etc/systemd/system/kubelet.service
+sudo mkdir -p /etc/systemd/system/kubelet.service.d
+curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | sudo tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+systemctl enable --now kubelet
